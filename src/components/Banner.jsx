fc0b7b10d91e2b42ff14b8f413429f4a59647755
Owen <contato.owen.br@gmail.com>
2025-01-27 19:54:33 -0300

    margin-top banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import sitePet from '../assets/site-pet.svg'
 
 export default function Banner() {
     return (
-            <div className="mb-36 m-auto gap-5 max-w-6xl flex justify-center items-center bg-primary text-white rounded-3xl max-sm:mx-3">
+            <div className="mt-56 mb-36 m-auto gap-5 max-w-6xl flex justify-center items-center bg-primary text-white rounded-3xl max-sm:mx-3">
                 <div className="bg-gray-500 h-[100%] rounded-3xl"><img src={sitePet} alt="Site PetShop" /></div>
                 <div className="">
                     <h3 className="text-3xl font-bold">Pra a sua empresa</h3>
